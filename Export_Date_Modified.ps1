9add0562d940edca332781e3602211db3a2df28a
Zach <zach.nybo@gmail.com>
2018-11-12 11:08:46 -0800

    Added Script Info

--- Export_Date_Modified.ps1
+++ Export_Date_Modified.ps1
@@ -1,1 +1,11 @@
+<#
+    Script : Export_Date_Modified.ps1
+    Updated: 11/12/2018
+    Author : Zach Nybo <zach.nybo@gmail.com>
+    Version: 1.0
+
+    Summary:
+    This script exports the "Date modified" for all files in a directory and
+    exports the information to a CSV.
+#>
 
